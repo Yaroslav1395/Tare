4ed2ef5a401ee0f22b2398bc6f1f1ad60c60fcd7
Ярослав <sakhno.yaroslav.13.95@gmail.com>
2024-09-03 21:56:17 +0600

    проверка на дубликаты характеристик

--- src/main/java/kg/zavod/Tare/repository/product/CharacteristicRepository.java
+++ src/main/java/kg/zavod/Tare/repository/product/CharacteristicRepository.java
@@ -4,6 +4,23 @@ import kg.zavod.Tare.domain.product.CharacteristicEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CharacteristicRepository extends JpaRepository<CharacteristicEntity, Integer> {
+
+    /**
+     * Метод позволяет найти характеристику по названию
+     * @param name - название
+     * @return - характеристика
+     */
+    Optional<CharacteristicEntity> findByName(String name);
+
+    /**
+     * Метод позволяет найти характеристику по названию исключая характеристику с переданным id
+     * @param name - название характеристики
+     * @param id - id характеристики
+     * @return - характеристика
+     */
+    Optional<CharacteristicEntity> findByNameAndIdIsNot(String name, Integer id);
 }
