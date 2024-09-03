b083160cf1a801d5d96332c778084d09e0c9edf6
Ярослав <sakhno.yaroslav.13.95@gmail.com>
2024-09-03 23:39:35 +0600

    добавлеение типа картики для подкатегории

--- src/main/java/kg/zavod/Tare/domain/category/CategoryEntity.java
+++ src/main/java/kg/zavod/Tare/domain/category/CategoryEntity.java
@@ -1,6 +1,7 @@
 package kg.zavod.Tare.domain.category;
 
 import jakarta.persistence.*;
+import kg.zavod.Tare.domain.ImageType;
 import kg.zavod.Tare.dto.exception.DeleteException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,6 +23,9 @@ public class CategoryEntity {
     private String name;
     @Column(name = "category_image")
     private String categoryImage;
+    @Column(name = "category_image_type")
+    @Enumerated(EnumType.STRING)
+    private ImageType imageType;
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     private List<SubcategoryEntity> subcategories;
 
