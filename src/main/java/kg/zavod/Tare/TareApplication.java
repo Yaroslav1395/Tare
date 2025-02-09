88a8e6d365ea311124e30194a538ecd5438f5e61
Ярослав <sakhno.yaroslav.13.95@gmail.com>
2025-02-09 16:38:52 +0600

    настройка локального запуска без докера и с докером безманипуляций

--- src/main/java/kg/zavod/Tare/TareApplication.java
+++ src/main/java/kg/zavod/Tare/TareApplication.java
@@ -2,7 +2,7 @@ package kg.zavod.Tare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//TODO: совместить ветку докер и внести в env данные для jwt
+
 @SpringBootApplication
 public class TareApplication {
 
